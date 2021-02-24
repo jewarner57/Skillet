86ed6d6185f28d6b594936fae66a90660f82987d
Jonathan Warner <jewarner57@gmail.com>
2021-02-23 20:11:17 -0500

    feat: finish initial project setup

--- skillet/auth/forms.py
+++ skillet/auth/forms.py
@@ -3,40 +3,43 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, DateField, SelectField, SubmitField, PasswordField
 from wtforms.ext.sqlalchemy.fields import QuerySelectField
 from wtforms.fields.core import FloatField
-from wtforms.validators import DataRequired, Length, URL, ValidationError
+from wtforms.validators import DataRequired, Length, URL, ValidationError, Email
 from skillet import bcrypt
 
 
 class SignUpForm(FlaskForm):
     """Form for creating a new user"""
+    email = StringField('Email',
+                        validators=[DataRequired(),
+                                    Length(min=3, max=50), Email()])
     username = StringField('User Name',
                            validators=[DataRequired(),
                                        Length(min=3, max=50)])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Sign Up')
 
-    def validate_username(self, username):
-        user = User.query.filter_by(username=username.data).first()
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
         if user:
             raise ValidationError(
-                'That username is taken. Please choose a different one.')
+                'That email is already associated with another account.')
 
 
 class LoginForm(FlaskForm):
     """Form for logging in a user"""
-    username = StringField('User Name',
-                           validators=[DataRequired(),
-                                       Length(min=3, max=50)])
+    email = StringField('Email',
+                        validators=[DataRequired(),
+                                    Length(min=3, max=50), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Log In')
 
-    def validate_username(self, username):
-        user = User.query.filter_by(username=username.data).first()
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
         if not user:
-            raise ValidationError('No user found with that username.')
+            raise ValidationError('No user found with that email.')
 
     def validate_password(self, password):
-        user = User.query.filter_by(username=self.username.data).first()
+        user = User.query.filter_by(email=self.email.data).first()
         if user and not bcrypt.check_password_hash(user.password,
                                                    password.data):
-            raise ValidationError('Password does not match.')
+            raise ValidationError('Invalid Username or Password')
